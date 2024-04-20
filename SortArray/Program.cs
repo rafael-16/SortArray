6d554fdc0898919f9dcfe7633ca497cbcf30bf08
Рафаэль Мухамедьяров <Rafael17IS16@gmail.com>
2024-04-20 13:39:38 +0300

    Add project files.

---
+++ SortArray/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Введите количество строк:");
+int row = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов:");
+int col = Convert.ToInt16(Console.ReadLine());
